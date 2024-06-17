9df4b7deadfaa0ba29b740a1906b43ab8020457e
성준혁 <146051592+jun0709@users.noreply.github.com>
2024-06-18 02:14:01 +0900

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -1,44 +1,18 @@
+$(function () {
+    $(".menu_box").click(function () {
+        $(".menu_btn").stop().slideToggle();
+        $(".menu_btn").css({
+            "width": "200px",
+            "height": "300px"
+        })
+    })
+    $('.main_nav_li').on('click', function () {
+        var index = $(this).index();
 
 
-    function initializeSwiper(selector) {
-        return new Swiper(selector, {
-            slidesPerView: 3,
-            spaceBetween: 30,
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true,
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-            autoplay: {
-                delay: 300,
-            },
-            speed: 3000,
-            loop: true,
-            direction: 'horizontal',
-            effect: 'coverflow'
-        });
-    }
-    
-    var swiperArray = [];
-    
-    for (let i = 1; i <= 6; i++) {
-        const swiper = initializeSwiper(`.mySwiper${i}`);
-        swiperArray.push(swiper);
-    
-        $(`#new_arr`).hover(function () {
-            swiper.autoplay.stop();
-        }, function () {
-            swiper.autoplay.start();
-        });
-    }
-    
-    $('.main_nav_li').click(function () {
-        const index = $(this).index();
-        console.log(index);
-        $('.mySwiper').removeClass('display_block');
-        $('.mySwiper').eq(index).addClass('display_block');
-        
+        $('.item_list').removeClass('active');
+
+
+        $('.item_list').eq(index).addClass('active');
     });
+})
