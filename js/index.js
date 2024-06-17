f9e514de50ba571d90ba7b48f2ca0f9db54856b1
성준혁 <146051592+jun0709@users.noreply.github.com>
2024-06-17 16:54:14 +0900

    index.js

--- js/index.js
+++ js/index.js
@@ -1,20 +1,7 @@
-const slider = document.querySelector('.slider');
+
 const openPopupBtn1 = document.getElementById('slide_pop1');
 const openPopupBtn2 = document.getElementById('slide_pop2');
 const openPopupBtn3 = document.getElementById('slide_pop3');
-setInterval(() => {
-    slider.style.transform = 'rotateY(90deg)';
-    setTimeout(() => {
-        slider.style.transform = 'rotateY(180deg)';
-        setTimeout(() => {
-            slider.style.transform = 'rotateY(270deg)';
-            setTimeout(() => {
-                slider.style.transform = 'rotateY(360deg)';
-            }, 100);
-        }, 100);
-    }, 100);
-}, 3000);
-
 openPopupBtn1.addEventListener('click', () => {
     window.location.href = 'story.html';
 })
